bf781628ebe6dafad03030345c5fbaf44e2a02c7
xiaowo <shaoyan.xsy@alibaba-inc.com>
2015-08-23 11:09:41 +0800

    add some explain

--- Lexical_analysis/include/color_token.h
+++ Lexical_analysis/include/color_token.h
@@ -2,4 +2,7 @@ enum e_LexState{
     LEX_NORMAL,
     LEX_SEP,
 };
+
+void color_token(int lex_state);
+
 extern char ch;
--- Lexical_analysis/include/exception_handler.h
+++ Lexical_analysis/include/exception_handler.h
@@ -10,6 +10,13 @@ enum e_WorkStage{
     STAGE_LINK,
 };
 
+void handle_exception(int stage,int level,char * fmt,va_list ap);
+void warning(char * fmt,...);
+void error(char * fmt,...);
+void expect(char *msg);
+void skip(int c);
+void link_error(char * fmt,...);
+
 extern FILE * fin;
 extern int line_num;
 extern DynArray tktable;
